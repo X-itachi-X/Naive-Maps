45c40a610a992190dcf8cdcb9dfd4ec68dabf41e
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-12 13:10:24 +0000

    Oct 12, 2023, 6:40 PM

--- src/App.js
+++ src/App.js
@@ -30,8 +30,7 @@ async function getCityLocation(cityName) {
 
     if (response.data.features.length > 0) {
       const location = response.data.features[0].center;
-      setLatitude(location[1]);
-      setLongitude(location[0]);
+      
       
       return { latitude, longitude };
     } else {
@@ -46,7 +45,8 @@ async function getCityLocation(cityName) {
 const cityName = 'New York'; // Replace with the city name you want to look up.
 getCityLocation(cityName)
   .then((location) => {
-    console.log('Location:', location);
+      setLatitude(location[1]);
+      setLongitude(location[0]);
   })
   .catch((error) => {
     console.error('Error:', error);
