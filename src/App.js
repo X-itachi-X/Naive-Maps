334daa62cb07e85e269256da3743590c1819e3ee
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-12 09:35:19 +0000

    Oct 12, 2023, 3:05 PM

--- src/App.js
+++ src/App.js
@@ -31,14 +31,21 @@ function App() {
         <div className='body-content'>
           <div className='box'>
             <div className='emptyDiv'></div>
-            
-            <form autocomplete="on">
+            <form>
+            <input name="address" placeholder="Address" type="text" />
+            <input name="unit" placeholder="Unit number" type="text" />
+            <input name="city" placeholder="City" type="text" />
+            <input name="state" placeholder="State" type="text" />
+            <input name="country" placeholder="Country" type="text" />
+            <input name="postcode" placeholder="Postcode" type="text" />
+        </form>
+            {/* <form autocomplete="on">
               <input className='textBox' type="text" placeholder='Starting Point'></input>
               <br></br>
               <input className='textBox' type="text" placeholder='Destination Point'></input>
               <br></br>
               <input type="submit" value="Submit" class="bn632-hover bn23"></input>
-            </form>
+            </form> */}
             
           </div>
         </div>
