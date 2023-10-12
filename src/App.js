f5d5b1f2f34eefac1591e2a8e594b919a939465c
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-12 10:24:39 +0000

    Oct 12, 2023, 3:54 PM

--- src/App.js
+++ src/App.js
@@ -1,39 +1,4 @@
 import './App.css';
-import React, { Component } from 'react';
-import axios from 'axios';
-
-
-class CityLocation extends Component {
-  state = {
-    cityName: '',
-    latitude: null,
-    longitude: null,
-  };
-
-  handleChange = (e) => {
-    this.setState({ cityName: e.target.value });
-  };
-
-  getCityLocation = () => {
-    const { cityName } = this.state;
-    const apiKey = 'YOUR_MAPBOX_API_KEY';
-
-    axios
-      .get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${cityName}.json?access_token=${apiKey}`)
-      .then((response) => {
-        if (response.data.features.length > 0) {
-          const location = response.data.features[0].center;
-          this.setState({ latitude: location[1], longitude: location[0] });
-        } else {
-          alert('City not found or there was an error.');
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  };
-
-
 
 function App() {
   return (
