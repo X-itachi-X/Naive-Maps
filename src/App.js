4c281cb64914ba7a7f1c44a946a4b8cd9ddd4510
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-12 09:51:55 +0000

    Oct 12, 2023, 3:21 PM

--- src/App.js
+++ src/App.js
@@ -11,6 +11,44 @@ const queryParams = {
   types: "address"
 };
 
+function parseReverseGeo(geoData) {
+  // debugger;
+  let locality, region, country, postcode, returnStr;
+  if (geoData.context) {
+    geoData.context.forEach((v, i) => {
+      if (v.id.indexOf("locality") >= 0) {
+        locality = v.text;
+      }
+      if (v.id.indexOf("postcode") >= 0) {
+        postcode = v.text;
+      }
+      if (v.id.indexOf("region") >= 0) {
+        region = v.text;
+      }
+      if (v.id.indexOf("country") >= 0) {
+        country = v.text;
+      }
+    });
+  }
+  if (postcode && region && country) {
+    returnStr = `${geoData.address} ${
+      geoData.text
+    }, ${locality} ${region} ${postcode}, ${country}`;
+  } else {
+    returnStr = geoData.place_name;
+  }
+  return {
+    number: geoData.address,
+    address: geoData.text,
+    locality,
+    region,
+    postcode,
+    country,
+    concat: returnStr,
+    complete: geoData.place_name
+  };
+}
+
 function _suggestionSelect(result, lat, long, text) {
   console.log(result, lat, long, text);
 };
