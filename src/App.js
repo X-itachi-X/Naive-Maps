ec952fd5682de2f9209852fc1764b5e502558849
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-12 11:43:52 +0000

    Oct 12, 2023, 5:13 PM

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
 import './App.css';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-
-function CityLocation(props) {
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-
-  const { cityName } = props;
-
-  // You can make the API call to get the latitude and longitude using the cityName
-  // Replace this with your actual API call logic
-  // For the sake of this example, we'll just set some dummy values:
-  setLatitude(40.7128);
-  setLongitude(-74.0060);
-
-  return (
-    <div>
-      <h2>City Location for {cityName}</h2>
-      {latitude !== null && longitude !== null && (
-        <div>
-          <p>Latitude: {latitude}</p>
-          <p>Longitude: {longitude}</p>
-        </div>
-      )}
-    </div>
-  );
-}
-
+import CityLocation from './components/CityLocation';
 
 function App() {
   const [cityName, setCityName] = useState('');
-
-  const handleCityNameChange = (e) => {
-    setCityName(e.target.value);
-  }
-
-  const handleGetLocation = () => {
-    // You can call an API or any other logic to fetch location data here.
-    // For this example, we'll set the cityName prop and receive location data directly.
-  }
-
   return (
     
     <div className="App">
 
-    <Router>
+      <Router>
       <header className="header-parent">
         <div className="header-background">
 
@@ -65,32 +30,22 @@ function App() {
             <div className='emptyDiv'></div>
             <form autocomplete="on">
               <input className='textBox' type="text" placeholder='Starting Point'></input>
-              <p>Latitude: {latitude}</p>
-              <p>Longitude: {longitude}</p>
               <br></br>
               <input className='textBox' type="text" placeholder='Destination Point'></input>
-              <p>Latitude: {latitude}</p>
-              <p>Longitude: {longitude}</p>
               <br></br>
               <input type="submit" value="Submit" class="bn632-hover bn23"></input>
             </form>
-            <input
-          type="text"
-          placeholder="Enter city name"
-          value={cityName}
-          onChange={(e) => setCityName(e.target.value)}
-        />
             
             <Link to="/city-location">city</Link>
               <Routes>
-                <Route path="/city-location" element={<CityLocation cityName={cityName}/>} />
+                <Route path="/city-location" element={<CityLocation />} />
               </Routes>
             
             
           </div>
         </div>
       </main>
-      </Router>
+    </Router>
     </div>
   );
 }
