0682e48c1823980a35056555e02ee5946a6f2ad2
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-12 02:18:29 +0000

    Oct 12, 2023, 7:48 AM

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header-parent">
+        <div className="header-background">
+
+        </div>
+        <div className="header-content">
+          
+        </div>
         
       </header>
     </div>
