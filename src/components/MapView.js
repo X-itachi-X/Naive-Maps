c0fb72aa58aed90de8b88de55120c8bb74999b17
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-13 04:41:19 +0000

    Oct 13, 2023, 10:11 AM

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 const Map = () => {
-  const mapboxToken = 'YOUR_MAPBOX_ACCESS_TOKEN'; // Replace with your actual token
+  const mapboxToken = 'sk.eyJ1IjoicmF2aXN1bWl0IiwiYSI6ImNsbm80YXVxcTBkaGsyam9kMThhZnp6eGMifQ.o_6GwvacW6sgzSlW9PLx8w'; // Replace with your actual token
 
   useEffect(() => {
     mapboxgl.accessToken = mapboxToken;
