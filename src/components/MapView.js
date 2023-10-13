f17eeeaf3fd616a9d4d1367d07355b146179067b
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-13 04:51:44 +0000

    Oct 13, 2023, 10:21 AM

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -19,7 +19,7 @@ const Map = () => {
     };
   }, [mapboxToken]);
 
-  return <div id="map-container" style={{ width: '100%', height: '200px' }} />;
+  return <div id="map-container" style={{ width: '100%', height: '300px' }} />;
 };
 
 export default Map;
