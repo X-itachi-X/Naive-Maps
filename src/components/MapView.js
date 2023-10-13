9100a04cf191b49d6ad5207f311c400a16cfd88f
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-13 03:04:33 +0000

    Oct 13, 2023, 8:34 AM

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import ReactMapboxGl from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-const Map = ReactMapboxGl({
-  accessToken: 'YOUR_MAPBOX_ACCESS_TOKEN',
-});
-
-const MapboxMap = () => {
-  const mapContainerStyle = {
-    height: '400px',
+function Map() {
+  const [viewport, setViewport] = useState({
     width: '100%',
-  };
+    height: '100%',
+    latitude: 37.7749, // Latitude of your desired location
+    longitude: -122.4194, // Longitude of your desired location
+    zoom: 10, // Zoom level (adjust as needed)
+  });
 
   return (
-    <div>
-      <h1>Mapbox Map</h1>
-      <Map
-        style="mapbox://styles/mapbox/streets-v11"
-        containerStyle={mapContainerStyle} // Use the mapContainerStyle object here
-      >
-        <div>Custom Map Content</div>
-      </Map>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken="YOUR_MAPBOX_ACCESS_TOKEN"
+      onViewportChange={(newViewport) => setViewport(newViewport)}
+      mapStyle="mapbox://styles/mapbox/streets-v11" // You can choose a different map style
+    />
   );
-};
+}
 
-export default MapboxMap;
+export default Map;
