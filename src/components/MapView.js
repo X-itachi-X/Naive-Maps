43819af320bfae28aaa5e071048c97da9bc36999
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-13 04:12:47 +0000

    Oct 13, 2023, 9:42 AM

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactMapGL from 'react-map-gl';
 
 const Map = () => {
-  const mapboxToken = 'sk.eyJ1IjoicmF2aXN1bWl0IiwiYSI6ImNsbm14Ymk0eTAwZnMyaXAxNmNoZGZocGUifQ.fZcPqWqoZXQhYQ-WmvdU5Q';
+  
   const initialViewport = {
     latitude: 40.7128, // Latitude of New York City
     longitude: -74.0060, // Longitude of New York City
