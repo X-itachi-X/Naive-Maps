8f2d70b9747512dfa1d8a738c577c1e37fb27043
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-14 15:31:40 +0000

    Oct 14, 2023, 9:01 PM

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -22,7 +22,7 @@ const Map = () => {
 
   return (
     
-      <div className='map' id="map-container" style={{ width: '100%', height: '350px' }} />
+      <div className='map' id="map-container" style={{ width: '95%', height: '350px' }} />
     
   
   );
