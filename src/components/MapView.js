88396ae07b47b77c7e8f5af5fb17660bd730b507
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-14 18:42:42 +0000

    Oct 15, 2023, 12:12 AM

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -13,14 +13,14 @@ function Map({lat=25,long=25}) {
     const map = new mapboxgl.Map({
       container: 'map-container', // The container ID where the map will be rendered
       style: 'mapbox://styles/ravisumit/clnipt7wy03yf01qu2iljfl8g', // You can choose a different map style
-      center: [-74.006, 40.7128], // Centered on New York City
+      center: [lat, long], // Centered on New York City
       zoom: 10, // Zoom level
     });
 
     return () => {
       map.remove(); // Clean up when the component is unmounted
     };
-  }, [mapboxToken]);
+  }, [lat, long, mapboxToken]);
 
   return (
     
