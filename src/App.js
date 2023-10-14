8a86cab62290ee11325196a054e79295874dd113
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-14 19:37:42 +0000

    Oct 15, 2023, 1:07 AM

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ getCityLocation(cityName1)
         <Link to="/Naive-Maps/map"><input type="submit" value="Submit" className="bn632-hover bn23" ></input></Link>
       </form>
       ) : (
-        <h2><MapView /></h2>
+        <h2>map</h2>
       )}
     </div>
 
