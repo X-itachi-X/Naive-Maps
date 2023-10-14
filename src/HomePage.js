2de26ea90085e8a739d8f8b1ee558279e510fd12
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-14 19:09:01 +0000

    Oct 15, 2023, 12:39 AM

--- src/HomePage.js
+++ src/HomePage.js
@@ -71,7 +71,7 @@ getCityLocation(cityName1)
   });
   return (
     <div>
-      {window.location.pathname !== '/Naive-Maps' ? (
+      {window.location.pathname === '/Naive-Maps' ? (
         <form>
         <input  className='textBox' 
                 type="text"
