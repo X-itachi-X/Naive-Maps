73618e06f5d6d75f85d274ac3af26c841e268822
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-14 18:22:50 +0000

    Oct 14, 2023, 11:52 PM

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 import "../App.css"
+import App from '../App';
 
 function Map() {
+
   const mapboxToken = 'pk.eyJ1IjoicmF2aXN1bWl0IiwiYSI6ImNsbmxxcWFsNjAyZ24yam4xanI1NDZ1NGoifQ.0meAZoM0gF_dSim2ZxrvJA'; // Replace with your actual token
 
   useEffect(() => {
@@ -11,7 +13,7 @@ function Map() {
     const map = new mapboxgl.Map({
       container: 'map-container', // The container ID where the map will be rendered
       style: 'mapbox://styles/ravisumit/clnipt7wy03yf01qu2iljfl8g', // You can choose a different map style
-      center: [-74.006, 40.7128], // Centered on New York City
+      center: [App.lat1, App.long1], // Centered on New York City
       zoom: 10, // Zoom level
     });
 
