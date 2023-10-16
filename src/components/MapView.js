bece88fc2facc31be6c514b8fe755850eb88746c
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-10-16 04:45:09 +0000

    Oct 16, 2023, 10:15 AM

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,69 +1,89 @@
-import React, { useEffect } from 'react';
-import mapboxgl from 'mapbox-gl';
+import React, { useEffect, useState } from "react";
+import mapboxgl from "mapbox-gl";
 
-import "../App.css"
-import 'mapbox-gl/dist/mapbox-gl.css';
-
+import "../App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
+  const letters = "0123456789ABCDEF";
+  let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
 
+function Map({ lat1, long1, lat2, long2 }) {
+  const mapboxToken =
+    "pk.eyJ1IjoicmF2aXN1bWl0IiwiYSI6ImNsbmxxcWFsNjAyZ24yam4xanI1NDZ1NGoifQ.0meAZoM0gF_dSim2ZxrvJA"; // Replace with your actual token
+  console.log(lat1, long1);
+  const [locations, setLocations] = useState([
+    {
+      name: 'Location 1',
+      latitude: lat1, // Replace with the first location's longitude
+      longitude: long1, // Replace with the first location's latitude
+    },
+    {
+      name: 'Location 2',
+      latitude: lat2, // Replace with the second location's longitude
+      longitude: long2, // Replace with the second location's latitude
+    },
+  ]);
+console.log(locations);
+  const calculateDistance = () => {
+    const { latitude: lat1, longitude: lon1 } = locations[0];
+    const { latitude: lat2, longitude: lon2 } = locations[1];
 
-function Map({lat,long}) {
-  
-  const mapboxToken = 'pk.eyJ1IjoicmF2aXN1bWl0IiwiYSI6ImNsbmxxcWFsNjAyZ24yam4xanI1NDZ1NGoifQ.0meAZoM0gF_dSim2ZxrvJA'; // Replace with your actual token
-  console.log(lat,long);
-  
+    // Haversine formula to calculate distance
+    const radius = 6371; // Earth radius in km
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) *
+        Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = radius * c;
+
+    return distance;
+    
+  };
+
   useEffect(() => {
     mapboxgl.accessToken = mapboxToken;
 
     const map = new mapboxgl.Map({
-      container: 'map', // The container ID where the map will be rendered
-      style: 'mapbox://styles/ravisumit/clnipt7wy03yf01qu2iljfl8g', // You can choose a different map style
-      center: [lat, long], // Centered on New York City
+      container: "map", // The container ID where the map will be rendered
+      style: "mapbox://styles/ravisumit/clnipt7wy03yf01qu2iljfl8g", // You can choose a different map style
+      center: [lat1, long1], // Centered on New York City
       zoom: 10, // Zoom level
     });
-    
 
-    
-
-
-   
-
-    map.on('click', (e) => {
+    map.on("click", (e) => {
       const { lng, lat } = e.lngLat;
       const randomColor = getRandomColor();
       // Create a custom marker element
-    
+
       // Set the marker's position
-      new mapboxgl.Marker({color: randomColor})
+      new mapboxgl.Marker({ color: randomColor })
         .setLngLat([lng, lat])
         .addTo(map);
-        console.log(`marker Set on ${lng},${lat}`);
+      console.log(`marker Set on ${lng},${lat}`);
     });
-   
-    
-      
-  
-  
 
     return () => {
       map.remove(); // Clean up when the component is unmounted
     };
-  }, [lat, long, mapboxToken]);
+  }, [lat1, long1, mapboxToken]);
 
   return (
-    
-      <div className='map' id="map" style={{ width: '90%', height: '50vh' }} />
-    
-  
+    <div>
+    <p>Distance: {calculateDistance().toFixed(2)} km</p>
+    <div className="map" id="map" style={{ width: "90%", height: "50vh" }} />
+    </div>
   );
-};
+}
 
 export default Map;
